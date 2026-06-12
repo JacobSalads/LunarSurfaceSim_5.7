7566db5de1c9b34be2a6cd463d603ea32cee4205
jacob <jactsalazar@outlook.com>
2026-06-11 21:31:17 -0500

    Updated world environment and image processing in the loop

---
+++ Content/Scripts/Python/Test/horizon_renderer.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:59f8bf36c8ed63953639a2aaca99df3402b0c52053fd021c23cb7a2466ae15f1
+size 22892
---
+++ Content/Scripts/Python/Test/test_exported_heightmap.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d092fbcdfa4a67964dd7a68b6fa6f8f8449e2e471be66627ccb8373128578e5c
+size 1453
