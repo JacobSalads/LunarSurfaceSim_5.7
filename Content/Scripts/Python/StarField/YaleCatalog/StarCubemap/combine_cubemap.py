d34c324322b004b0fad0dd7c5924600af7bc65bd
jacob <jactsalazar@outlook.com>
2026-06-28 21:15:07 -0500

    SPICE starfield development, rover self-driving added, time adjustment during runtime.

---
+++ Content/Scripts/Python/StarField/YaleCatalog/StarCubemap/combine_cubemap.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:43e71dec90f3fe98ec31d17b76621341ac945ee56e076861769b350e3b99da86
+size 2058
---
+++ Content/Scripts/Python/StarField/YaleCatalog/StarCubemap/cubemap_to_ue5.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d7242572ed14b8ced8455bdb097fcc05e8e735755e9ca380d844eb27ec5acd8d
+size 4018
